f2bfb8f43e357605c00ffab279d3eb72bad7a95c
ethandamian <ethandamian@ciencias.unam.mx>
2023-11-09 21:50:36 -0600

    First commit

---
+++ SQL/Query.sql
@@ -0,0 +1,21 @@
+-- Los veterinarios y cuidadores que comparten el apellido.
+
+
+-- Los animales herbivoros que pertenezcan al bioma bosque templado.
+
+
+-- Alimentos de tipo semilla, que son distribuidos en el aviario.
+
+
+-- Los cuidadores y proveedores que vivan en el mismo estado.
+
+
+-- Biomas que posean al menos 10 animales.
+SELECT *
+FROM Bioma
+WHERE IDBioma IN (
+    SELECT IDBioma
+    FROM Animal
+    GROUP BY IDBioma
+    HAVING COUNT(*) >= 10
+); 
